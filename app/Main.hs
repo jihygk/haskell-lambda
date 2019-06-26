0a8a1cb035376da52410ac5203516064dca842f1
Jay Kim <jihygk@gmail.com>
2019-06-26 17:14:28 +1000

    fix proper handling of schedule event and timing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import qualified Data.Aeson as Aeson
+import AWSLambda
+import AWSLambda.Events
 
 main :: IO ()
-main = run
+main = lambdaMain handler
+
+
+handler :: Aeson.Value -> IO ()
+handler event = run
+
